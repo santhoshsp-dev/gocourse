6b84bd2af85b035d32982baf393a75bec4aa2839
santhoshsp-dev <99028127+santhoshsp-dev@users.noreply.github.com>
2026-07-23 17:55:24 +0530

    add variables

---
+++ basics/009_variables.go
@@ -0,0 +1,19 @@
+package basics
+package main
+
+import "fmt"
+
+var middleName = "cane"
+
+func main() {
+	// var age int
+	// var name string = "san"
+	// var name1 = "Jane"
+	middleName := "SP"
+	fmt.Println(middleName)
+	// fmt.Println(firstName)
+}
+func printName() {
+	firstName := "Michel"
+	fmt.Println(firstName)
+}
