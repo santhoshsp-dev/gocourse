9e2e4ea8b010703e7d12b26493ebc2e5adbf0572
Santhosh SP <99028127+santhoshsp-dev@users.noreply.github.com>
2026-08-03 08:21:06 +0000

    add channel directions

---
+++ advanced/006_channel_directions.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+// Step: 1)
+// SEND CHANNEL
+// func main() {
+// 	ch := make(chan int)
+
+// 	go func(ch chan<- int) {
+// 		for i := range 5 {
+// 			ch <- i
+// 		}
+// 		close(ch)
+// 	}(ch)
+
+// 	for value := range ch {
+// 		fmt.Println("Received: ", value)
+// 	}
+// }
+
+// Step: 2)
+// RECEIVE CHANNEL
+// func main() {
+// 	ch := make(chan int)
+
+// 	go func() {
+// 		for i := range 5 {
+// 			ch <- i
+// 		}
+// 		close(ch)
+// 	}()
+
+// 	// for value := range ch {
+// 	// 	fmt.Println("Received: ", value)
+// 	// }
+// 	receiveData(ch)
+// }
+
+// func receiveData(ch <-chan int) {
+// 	for value := range ch {
+// 		fmt.Println("Received: ", value)
+// 	}
+// }
+
+// Step: 3)
+func main() {
+
+	ch := make(chan int)
+	producer(ch)
+	consumer(ch)
+}
+
+// Send only channel
+func producer(ch chan<- int) {
+	go func() {
+		for i := range 5 {
+			ch <- i
+		}
+		close(ch)
+	}()
+}
+
+// Receive only channel
+func consumer(ch <-chan int) {
+	for value := range ch {
+		fmt.Println("Received: ", value)
+	}
+}
