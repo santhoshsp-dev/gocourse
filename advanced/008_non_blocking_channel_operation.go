c3b66350fe06d4189009ec22f7797f4f06122a00
Santhosh SP <99028127+santhoshsp-dev@users.noreply.github.com>
2026-08-04 04:06:35 +0000

    add non blocking channel operation

---
+++ advanced/008_non_blocking_channel_operation.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// Step: 1)
+	// ch := make(chan int)
+
+	// === NON BLOCKING RECEIVE OPERATION
+	// select {
+	// case msg := <-ch:
+	// 	fmt.Println("Received:", msg)
+	// default:
+	// 	fmt.Println("No messages available.")
+	// }
+
+	// Step: 2)
+	// // === NON BLOCKING SEND OPERATION
+	// select {
+	// case ch <- 1:
+	// 	fmt.Println("Sent message.")
+	// default:
+	// 	fmt.Println("Channel is not ready to receive.")
+	// }
+
+	// Step: 3)
+	// === NON BLOCKING OPERATION IN REAL TIME SYSTEMS
+	data := make(chan int)
+	quit := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case d := <-data:
+				fmt.Println("Data received:", d)
+			case <-quit:
+				fmt.Println("Stopping...")
+				return
+			default:
+				fmt.Println("Waiting for data...")
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+
+	for i := range 5 {
+		data <- i
+		time.Sleep(time.Second)
+	}
+
+	quit <- true
+
+}
