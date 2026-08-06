6d4c52dc22f5afa46c9b90a2db8833a3da53a513
Santhosh SP <99028127+santhoshsp-dev@users.noreply.github.com>
2026-08-06 13:11:50 +0000

    add rate limiting fixed window algorithm

---
+++ advanced/019_rate_limiting_fixed_window_algo.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type RateLimiter struct {
+	mu        sync.Mutex
+	count     int
+	limit     int
+	window    time.Duration
+	resetTime time.Time
+}
+
+func NewRateLimiter(limit int, window time.Duration) *RateLimiter {
+	return &RateLimiter{
+		limit:  limit,
+		window: window,
+	}
+}
+
+func (rl *RateLimiter) Allow() bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	now := time.Now()
+	if now.After(rl.resetTime) {
+		rl.resetTime = now.Add(rl.window)
+		rl.count = 0
+	}
+
+	if rl.count < rl.limit {
+		rl.count++
+		return true
+	}
+	return false
+}
+
+func main() {
+	var wg sync.WaitGroup
+	rateLimiter := NewRateLimiter(3, 3*time.Second)
+
+	for range 10 {
+		wg.Add(1)
+		go func() {
+			if rateLimiter.Allow() {
+				fmt.Println("Request allowd")
+			} else {
+				fmt.Println("Request denied")
+			}
+			wg.Done()
+		}()
+		time.Sleep(200 * time.Millisecond)
+	}
+	wg.Wait()
+}
