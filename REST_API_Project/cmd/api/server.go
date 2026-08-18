2ebb51180dfc7dd274eef48d17cb470819261ca8
Santhosh SP <99028127+santhoshsp-dev@users.noreply.github.com>
2026-08-18 11:46:11 +0000

    035 Middlewares - Rate Limiter

--- REST_API_Project/cmd/api/server.go
+++ REST_API_Project/cmd/api/server.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 	mw "restapi/internal/api/middlewares"
-	"strings"
+	"time"
 )
 
 type user struct {
@@ -17,38 +17,20 @@ type user struct {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Root Route"))
-	fmt.Println("Hello Root Rute")
 }
 
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		fmt.Println(r.URL.Path)
-		path := strings.TrimPrefix(r.URL.Path, "/teachers/")
-		userID := strings.TrimSuffix(path, "/")
-		fmt.Println("The ID is:", userID)
-
-		fmt.Println("Query Params:", r.URL.Query())
-		queryParams := r.URL.Query()
-		sortby := queryParams.Get("sortby")
-		key := queryParams.Get("key")
-		sortorder := queryParams.Get("sortorder")
-
-		fmt.Printf("Sortby: %v, Sort order: %v, Key: %v", sortby, sortorder, key)
 		w.Write([]byte("Hello GET Method on Teachers Route"))
-		// fmt.Println("Hello GET Method on Teachers Route")
 	case http.MethodPost:
 		w.Write([]byte("Hello POST Method on Teachers Route"))
-		fmt.Println("Hello POST Method on Teachers Route")
 	case http.MethodPut:
 		w.Write([]byte("Hello PUT Method on Teachers Route"))
-		fmt.Println("Hello PUT Method on Teachers Route")
 	case http.MethodPatch:
 		w.Write([]byte("Hello PATCH Method on Teachers Route"))
-		fmt.Println("Hello PATCH Method on Teachers Route")
 	case http.MethodDelete:
 		w.Write([]byte("Hello DELETE Method on Teachers Route"))
-		fmt.Println("Hello DELETE Method on Teachers Route")
 	}
 
 }
@@ -57,19 +39,14 @@ func studentsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello GET Method on Students Route"))
-		fmt.Println("Hello GET Method on Students Route")
 	case http.MethodPost:
 		w.Write([]byte("Hello POST Method on Students Route"))
-		fmt.Println("Hello POST Method on Students Route")
 	case http.MethodPut:
 		w.Write([]byte("Hello PUT Method on Students Route"))
-		fmt.Println("Hello PUT Method on Students Route")
 	case http.MethodPatch:
 		w.Write([]byte("Hello PATCH Method on Students Route"))
-		fmt.Println("Hello PATCH Method on Students Route")
 	case http.MethodDelete:
 		w.Write([]byte("Hello DELETE Method on Students Route"))
-		fmt.Println("Hello DELETE Method on Students Route")
 	}
 }
 
@@ -77,19 +54,14 @@ func execsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello GET Method on Execs Route"))
-		fmt.Println("Hello GET Method on Execs Route")
 	case http.MethodPost:
 		w.Write([]byte("Hello POST Method on Execs Route"))
-		fmt.Println("Hello POST Method on Execs Route")
 	case http.MethodPut:
 		w.Write([]byte("Hello PUT Method on Execs Route"))
-		fmt.Println("Hello PUT Method on Execs Route")
 	case http.MethodPatch:
 		w.Write([]byte("Hello PATCH Method on Execs Route"))
-		fmt.Println("Hello PATCH Method on Execs Route")
 	case http.MethodDelete:
 		w.Write([]byte("Hello DELETE Method on Execs Route"))
-		fmt.Println("Hello DELETE Method on Execs Route")
 	}
 }
 
@@ -104,24 +76,24 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", rootHandler)
-
 	mux.HandleFunc("/teachers/", teachersHandler)
-
 	mux.HandleFunc("/students/", studentsHandler)
-
 	mux.HandleFunc("/execs/", execsHandler)
 
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 	}
+	// Start ---------- 035 -----------
+	rl := mw.NewRateLimiter(5, time.Minute)
+	// END ----------- 035 ------------
 
 	// Create custom server
 	server := &http.Server{
 		Addr: port,
-		// Start ---------- 034 -----------
-		Handler: mw.Compression(mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Cors(mux)))),
+		// Start ---------- 035 -----------
+		Handler: rl.Middleware(mw.Compression(mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Cors(mux))))),
+		// END ----------- 035 ------------
 		// Handler: middlewares.Cors(mux),
-		// END ----------- 034 ------------
 		TLSConfig: tlsConfig,
 	}
 
